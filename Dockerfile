07e48d6e21d7a384a8ade991064818142a2ce6e3
Tim Buchwaldt <tim@buchwaldt.ws>
2017-11-17 16:56:02 +0100

    Read data from k8s

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM resin/rpi-raspbian
 RUN apt update
 RUN apt-get install python3-pip python3-dev build-essential
-RUN pip3 install unicornhat
+RUN pip3 install unicornhat kubernetes
 ADD demo.py /demo.py
 ENTRYPOINT ["/demo.py"]
--- demo.py
+++ demo.py
@@ -5,6 +5,9 @@ import math
 import time
 
 import unicornhat as unicorn
+from kubernetes import client, config
+config.load_incluster_config()
+v1 = client.CoreV1Api()
 
 
 unicorn.set_layout(unicorn.HAT)
@@ -12,22 +15,38 @@ unicorn.rotation(0) # tested on pHAT/HAT with rotation 0, 90, 180 & 270
 unicorn.brightness(1)
 u_width,u_height=unicorn.get_shape()
 
-import socket
-
-
-IP = '0.0.0.0'
-PORT = 2808
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((IP, PORT))
+def set_val(myval):
+    unicorn.clear()
+    unicorn.show()
+    for w in range(0, u_width-1):
+        for h in range(0, u_height-1):
+            if myval > 0:
+                myval = myval-1
+                unicorn.set_pixel(w, h, 255, 255, 255)
+    unicorn.show()
 
 while True:
-    data, addr = s.recvfrom(5) # 1 byte for LED, 1 for R/G/B
-    ba = bytearray(data)
-    w = ba[0]
-    h = ba[1]
-    r = ba[2]
-    g = ba[3]
-    b = ba[4]
-    unicorn.set_pixel(w, h, r, g, b)
-    unicorn.show()
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    set_val(len(ret.items))
+    time.sleep(2)
+
+#
+#import socket
+#
+#
+#IP = '0.0.0.0'
+#PORT = 2808
+#
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.bind((IP, PORT))
+#
+#while True:
+#    data, addr = s.recvfrom(5) # 1 byte for LED, 1 for R/G/B
+#    ba = bytearray(data)
+#    w = ba[0]
+#    h = ba[1]
+#    r = ba[2]
+#    g = ba[3]
+#    b = ba[4]
+#    unicorn.set_pixel(w, h, r, g, b)
+#    unicorn.show()
